66c2cd0e9cd211dd9af2fe86fca7ffb68ded960e
mischb <mischarberlin@gmail.com>
2018-10-08 14:49:05 -0400

    added static server

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 'use strict';
 
 // Load array of notes
+const express = require('express');
+const app = express();
 const data = require('./db/notes');
 
-console.log('Hello Noteful!');
+console.log('Hello World!');
 
+app.use(express.static('public'));
 // INSERT EXPRESS APP CODE HERE...
+// ADD STATIC SERVER HERE
+// app.get('/', (req,res) => {
+
+// });
+
+app.listen(8080, function(){
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
