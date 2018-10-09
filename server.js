8d1db5abed7cc306d5b6a5499d3883db33f64a73
mischb <mischarberlin@gmail.com>
2018-10-09 15:34:49 -0400

    added 404 error handling

--- server.js
+++ server.js
@@ -30,6 +30,20 @@ app.get('/api/notes/:id', (req, res) => {
   res.json(item);
 });
 
+app.use(function (req, res, next){
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found'});
+});
+
+app.use(function(err, req, res, next){
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
 app.listen(PORT, function(){
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
